c110453b159d9ee0108ae22fc3f435c7b8aaacef
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-07-08 14:00:24 +0700

    feat: update sha for nur

--- home-manager/nur.nix
+++ home-manager/nur.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: let
-  nurpkgs = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {inherit pkgs;};
+  nurpkgs = import (builtins.fetchTarball {
+    url = "https://github.com/nix-community/NUR/archive/master.tar.gz";
+    sha256 = "sha256:0yv7viid6x750l3384p77mk0carh53lqlpip6yaxambhqd2icrv3";
+  }) {inherit pkgs;};
 in {
   home.packages = with nurpkgs.repos; [
     LuisChDev.nordvpn
